ca77c56f0d6e654bf700394f0ee8a2a955c67bd4
“shashwat-sashi“ <shashwat_shandilya@epam.com>
2025-05-17 17:14:07 +0530

    4th

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,8 +1,3 @@
-data "azurerm_key_vault_secret" "sql_admin_username" {
-  name         = var.sql_admin_secret_name
-  key_vault_id = var.kv_id
-}
-
 # Use the secret directly in the connection string
 output "sql_connection_string" {
   description = "SQL Connection String for ADO.NET clients"
@@ -10,7 +5,7 @@ output "sql_connection_string" {
     "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
     azurerm_mssql_server.sql.fully_qualified_domain_name,
     azurerm_mssql_database.sql_db.name,
-    data.azurerm_key_vault_secret.sql_admin_username.value,
+    local.sql_admin_username,
     random_password.sql_password.result
   )
   sensitive = true
